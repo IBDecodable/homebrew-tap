5eb067d27c6269fac76ddf90c846328651117332
Yuta Saito <kateinoigakukun@gmail.com>
2018-08-13 23:29:43 +0900

    iblinter 0.4.0

--- iblinter.rb
+++ iblinter.rb
@@ -1,8 +1,8 @@
 class Iblinter < Formula
   desc "A linter tool for Interface Builder"
   url "https://github.com/IBDecodable/IBLinter.git",
-      :tag => "0.3.0",
-      :revision => "1135c1e3b2ccbc5f07043b5203f8361486ff3b06"
+      :tag => "0.4.0",
+      :revision => "5e8362578551956d4b9619803fd01d71864b9956"
   head "https://github.com/IBDecodable/IBLinter.git"
 
   def install
