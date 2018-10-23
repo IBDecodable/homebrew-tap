ba8c404dd6d3b0da0fac7b91f479947a0b0d9083
Yuta Saito <kateinoigakukun@gmail.com>
2018-10-23 10:06:57 +0900

    iblinter 0.4.7

--- iblinter.rb
+++ iblinter.rb
@@ -1,8 +1,8 @@
 class Iblinter < Formula
   desc "A linter tool for Interface Builder"
   url "https://github.com/IBDecodable/IBLinter.git",
-      :tag => "0.4.6",
-      :revision => "df2cd0978ab5d8535926b4781bd74a506effc862"
+      :tag => "0.4.7",
+      :revision => "8abbea5c7b3cb3ebbc9bbe772266f9236a9ebfd2"
   head "https://github.com/IBDecodable/IBLinter.git"
 
   depends_on "libxml2"
