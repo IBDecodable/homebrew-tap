60b3e5638ff1e09f175444758d4e090574d316f8
Yuta Saito <kateinoigakukun@gmail.com>
2019-04-15 23:33:40 +0900

    Support bottle

--- iblinter.rb
+++ iblinter.rb
@@ -6,6 +6,12 @@ class Iblinter < Formula
       :revision => "00d0f13a522708346d1578720545cdd469934e33"
   head "https://github.com/IBDecodable/IBLinter.git"
 
+  bottle do
+    root_url "https://github.com/IBDecodable/IBLinter/releases/download/0.4.15"
+    cellar :any_skip_relocation
+    sha256 "205f06fcef18ef56fcae24b21614261f24ca16f4fa154c31c39988d8ee89c73f" => :mojave
+  end
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
